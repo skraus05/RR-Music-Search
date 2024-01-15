cfedf547ece8457dbfc225334350832ed2cfd046
Samuel Kraus <samuelkraus@Samuels-MBP.lan>
2024-01-15 12:25:23 -0500

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, Fragment } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
 
 function App() {
 	let [search, setSearch] = useState('')
@@ -32,13 +35,22 @@ function App() {
 
 	return (
 		<div>
-			<SearchBar handleSearch = {handleSearch}/>
 			{message}
-			<Gallery data={data} />
+			<Router>
+				<Routes>
+					<Route path="/" element={
+						<Fragment>
+							<SearchBar handleSearch = {handleSearch}/>
+							<Gallery data={data} />
+							</Fragment>
+					} />
+					<Route path="/album/:id" element={<AlbumView />} />
+					<Route path="/artist/:id" element={<ArtistView />} />
+			</Routes>
+			</Router>
 		</div>
   	);
 }
 
 export default App;
 
-//doing something
